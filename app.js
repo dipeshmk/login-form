dbe8d4739ae9cbf5b57fd7104828e8659b67d027
dipeshkarkimk <dipeshkarkimk@gmail.com>
2024-07-22 21:24:21 +0545

    added updated files

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+function togglePassword(id) {
+    const passwordField = document.getElementById(id);
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
+}
